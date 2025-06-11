3dafb5ad7fe2876c088e2246e4e6479ca3aff06e
tayla <tayla.saab@aluno.senai.br>
2025-06-11 14:50:50 -0300

    Add Prontuario Eletronico

--- ProjetoClinicaMedica.Domain/Class1.cs
+++ ProjetoClinicaMedica.Domain/Class1.cs
@@ -1,4 +1,6 @@
-﻿namespace ProjetoClinicaMedica.Domain
+﻿using Microsoft.Win32;
+
+namespace ProjetoClinicaMedica.Domain
 {
     public class Class1
     {
@@ -20,5 +22,15 @@
         public DateTime DataNascimento { get; set; }
         public string HistoricoMedico { get; set; }
         public string ContatoEmergencia { get; set; }
+    }
+
+    public class Prontuario
+    {
+        public int Id { get; set; }
+        public DateTime DataConsuta { get; set; }
+        public DateTime HorarioConsuta { get; set; }
+        public string Medico { get; set; }
+        public string Exames { get; set; }
+        public string Diagnósticos { get; set; }
     }
 }
