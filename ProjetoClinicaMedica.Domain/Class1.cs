a9cbfe959bcafbb3b6f5ec1d5df18da07066b4e3
Lavzzn <lavinia.dassoller@aluno.senai.br>
2025-06-11 14:44:41 -0300

    Projeto Domain

---
+++ ProjetoClinicaMedica.Domain/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjetoClinicaMedica.Domain
+{
+    public class Class1
+    {
+
+    }
+    public class Usuario
+    {
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string HistoricoMedico { get; set; }
+        public string ContatoEmergencia { get; set; }
+    }
+}
