fc1c2c813aa263e3a0e5a8f90a3bac50613dd0ae
Lavzzn <lavinia.dassoller@aluno.senai.br>
2025-06-13 11:12:33 -0300

    atualizei

--- ProjetoClinicaMedicaa.Shared/UpdateProntuarioDto.cs
+++ ProjetoClinicaMedicaa.Shared/UpdateProntuarioDto.cs
@@ -8,5 +8,6 @@
         public string Medico { get; set; }
         public string Exames { get; set; }
         public string Diagnosticos { get; set; }
+        public string PacienteNome { get; set; }
     }
 }
