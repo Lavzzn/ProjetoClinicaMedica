81d55785334acdafe628d43233f9359bbfa41698
Lavzzn <lavinia.dassoller@aluno.senai.br>
2025-06-13 10:14:40 -0300

    ll

---
+++ ProjetoClinicaMedica.API/Controllers/AuthController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ProjetoClinicaMedica.API.Data;
+using ProjetoClinicaMedica.Domain;
+using ProjetoClinicaMedicaa.Shared;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ProjetoClinicaMedica.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ClinicaContext _context;
+        private readonly IConfiguration _configuration;
+        private readonly IPasswordHasher<Usuario> _passwordHasher;
+        public AuthController
+                       (ClinicaContext context,
+                       IConfiguration configuration,
+                       IPasswordHasher<Usuario> passwordHasher)
+        {
+            _context = context;
+            _configuration = configuration;
+            _passwordHasher = passwordHasher;
+        }
+
+        [HttpPost("register")]
+
+        public async Task<IActionResult> Register([FromBody] UserRegister model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new Usuario
+            {
+                Nome = model.Nome,
+                Role = model.Role,
+                Username = model.Username
+                
+            };
+
+            user.PasswordHash = _passwordHasher.HashPassword(user, model.Password);
+
+            _context.Usuarios.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { Message = "Usuário registrado com sucesso." });
+
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLogin userLogin)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _context.Usuarios.Where(e => e.Username == userLogin.Username)
+                .Select(u => new { u.Username, u.PasswordHash, u.Nome, u.Role }).FirstOrDefaultAsync();
+
+            if (user == null || _passwordHasher.VerifyHashedPassword
+                (null, user.PasswordHash, userLogin.Password) == PasswordVerificationResult.Failed)
+            {
+                return Unauthorized(new { Message = "Credenciais inválidas" });
+            }
+
+            var token = GenerateJwtToken(user.Username, user.Nome, user.Role);
+
+            return Ok(token);
+        }
+        private string GenerateJwtToken(string username, string nome, string role)
+        {
+            var claims = new[]
+            {
+        new Claim ("nome", "valor"),
+        new Claim(ClaimTypes.Name, nome),
+        new Claim(ClaimTypes.NameIdentifier, username),
+        new Claim(ClaimTypes.Role, role) // Include role in the token
+        };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(_configuration["Jwt:ExpireMinutes"])),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
+
---
+++ ProjetoClinicaMedica.API/Migrations/20250613123946_init.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoClinicaMedica.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medicos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Numero = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NomeCompleto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AreaAtuacao = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Numero = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HistoricoMedico = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContatoEmergencia = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prontuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Medico = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Exames = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Diagnósticos = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prontuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Consultas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataHora = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Valor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsCovenio = table.Column<bool>(type: "bit", nullable: false),
+                    Convenio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MedicoId = table.Column<int>(type: "int", nullable: false),
+                    MedicoId1 = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    PacienteId = table.Column<int>(type: "int", nullable: false),
+                    PacienteId1 = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consultas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Consultas_Medicos_MedicoId1",
+                        column: x => x.MedicoId1,
+                        principalTable: "Medicos",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Consultas_Pacientes_PacienteId1",
+                        column: x => x.PacienteId1,
+                        principalTable: "Pacientes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultas_MedicoId1",
+                table: "Consultas",
+                column: "MedicoId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultas_PacienteId1",
+                table: "Consultas",
+                column: "PacienteId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consultas");
+
+            migrationBuilder.DropTable(
+                name: "Prontuarios");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Medicos");
+
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+        }
+    }
+}
