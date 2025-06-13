28428a3535a1564c1f07a13cbf3f247aac64859a
bia g <beatriz.g.lourenco@aluno.senai.br>
2025-06-13 09:23:25 -0300

    pasta data

--- ProjetoClinicaMedicaa.Shared/UpDatePaciente.cs
+++ ProjetoClinicaMedicaa.Shared/UpDatePaciente.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProjetoClinicaMedicaa.Shared
 {
-    internal class UpdatePaciente
+    public class UpdatePaciente
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Nome { get; set; } = string.Empty;
