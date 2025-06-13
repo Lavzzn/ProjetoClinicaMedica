dbd14b1b1a3a2191cdc6d86ac35ee9a90c6a00f6
Lavzzn <lavinia.dassoller@aluno.senai.br>
2025-06-13 09:00:43 -0300

    kj

--- ProjetoClinicaMedicaa.Shared/CreateProntuario.cs
+++ ProjetoClinicaMedicaa.Shared/CreateProntuario.cs
@@ -5,7 +5,8 @@ namespace ProjetoClinicaMedicaa.Shared
     public class CreateProntuario
     {
         [Required]
-        public string Medico { get; set; }
+        public string MedicoNome { get; set; }
+        [Required]
         public string Exames { get; set; }
         public string Diagnosticos { get; set; }
     }
