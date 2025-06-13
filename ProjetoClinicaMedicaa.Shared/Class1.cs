7154fae485147128b1db41f417fd891748bfb775
Tayla <tayla.saab@aluno.senai.br>
2025-06-13 08:08:03 -0300

    Add Login

--- ProjetoClinicaMedicaa.Shared/Class1.cs
+++ ProjetoClinicaMedicaa.Shared/Class1.cs
@@ -1,4 +1,6 @@
-﻿namespace ProjetoClinicaMedicaa.Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoClinicaMedicaa.Shared
 {
     public class Class1
     {
@@ -10,5 +12,27 @@
         public string Nome { get; set; }
         public string AreaAtuacao { get; set; }
     }
+    public class UserRegister
+    {
+        [Required]
+        public string Nome { get; set; }
 
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(4, ErrorMessage = "A senha deve ter pelo menos 4 caracteres")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Senha e Confirmação de Senha não coicidem")]
+        public string ConfirmPassword { get; set; }
+
+        public string AreaAtuacao { get; set; } = "User";
+    }
+    public class UserLogin
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
 }
